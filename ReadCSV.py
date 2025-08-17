9e11e58e7669bfdceab3793ce0e3c4d59a890f54
Vishnu <vishnupriya0148@gmail.com>
2025-08-17 10:53:16 +0530

    read logic

--- ReadCSV.py
+++ ReadCSV.py
@@ -1,1 +1,13 @@
-#code for reading CSV
+import csv
+import os
+csv_file = input("enter tye path to your csv file")
+if not os.path.exists(csv_file):
+    print("File not found")
+else:
+    data = []
+    with open (csv_file,r,newline='',encoding='utf-8')as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data.append(row)
+    for row in data:
+        print(row)
